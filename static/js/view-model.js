6aecf6d88bd5ee5f45577e0c6ba5728f4e70b196
mingyangShang <shang_ming_yang@163.com>
2017-08-12 01:01:29 +0800

    fix 3dview display path error

--- static/js/view-model.js
+++ static/js/view-model.js
@@ -19,11 +19,9 @@ $(document).ready(function(){
 	});
 	$('#instructions').hide();
 	$('#instructionsPanel').click(function () { $('#instructions').toggle(); });
-	showModel("/static/airplane.off", $("#canvas"));
-	// showModel("{{url_for('static', filename='airplane.off')}}", $("#canvas"));
+	showModel('/static/airplane.off', $("#canvas"));
     featureChart = refreshFeatureChart("featureChart", [], []); //特征可视化
 
-    // showModel("three-vtk/models/vtk/airplane.off", $("#canvas"));
     // controls.enabled = true;
     window.controls.enabled = true;
     $("#canvas canvas").css("margin-top", "60px");
