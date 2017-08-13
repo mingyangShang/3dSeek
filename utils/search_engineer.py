5d48656626455291197c080adeed992b45a60964
wangxiyang <ssdutwxy@foxmail.com>
2017-08-13 13:08:51 +0800

    add wxy

--- utils/search_engineer.py
+++ utils/search_engineer.py
@@ -3,11 +3,16 @@ from sklearn.neighbors import KDTree
 from sklearn.externals import joblib
 import json
 
-model_name_list = json.loads('/home/wangxiyang/workspace/3dSeek/static/database/modelnet40/train_model_list.json')
+#model_name_list = load_json('/home/wangxiyang/workspace/3dSeek/static/database/modelnet40/train_model_list.json')
 lhl_kd_tree = joblib.load('/home/wangxiyang/workspace/3dSeek/methods/lhl/kd_tree/kd_tree.pkl')
-smy_kd_tree = joblib.load('/home/wangxiyang/workspace/3dSeek/methods/lhl/kd_tree/kd_tree.pkl')
-wxy_kd_tree = joblib.load('/home/wangxiyang/workspace/3dSeek/methods/lhl/kd_tree/kd_tree.pkl')
+smy_kd_tree = joblib.load('/home/wangxiyang/workspace/3dSeek/methods/smy/kd_tree/kd_tree.pkl')
+wxy_kd_tree = joblib.load('/home/wangxiyang/workspace/3dSeek/methods/wxy/kd_tree/kd_tree.pkl')
 
+def load_json(json_path):
+    with open(json_path) as fp:
+        return json.load(fp)
+
+model_name_list = load_json('/home/wangxiyang/workspace/3dSeek/static/database/modelnet40/train_model_list.json')
 
 def search_by_feature(search_feature, method_name='wxy', data_set='modelnet40'):
     """
@@ -25,7 +30,10 @@ def search_by_feature(search_feature, method_name='wxy', data_set='modelnet40'):
     elif method_name == 'lhl':
         kd_tree = lhl_kd_tree
     dist, ind = kd_tree.query(search_feature, 50)
-    return model_name_list[ind]
+    ind = ind.reshape([-1])
+    name_list = [model_name_list[int(i)] for i in ind]
+    
+    return name_list
 
     #return ['bathtub_0007' for i in range(50)]
 
@@ -38,9 +46,14 @@ def k_d_tree_test():
 
 
 def k_d_tree_dump():
-    X = np.load('train_feature_modelnet40.npy')
+    X = np.load('/home/wangxiyang/workspace/3dSeek/methods/lhl/train_feature_modelnet40.npy')
     tree = KDTree(X)
-    joblib.dump(tree, 'kd_tree/kd_tree.pkl')
+    dist, ind = tree.query(X[0], 10)
+    ind = ind.reshape([-1])
+    print(ind)
+    name_list = [model_name_list[int(i)] for i in ind]
+    print(name_list)
+    #joblib.dump(tree, 'kd_tree/kd_tree.pkl')
 
 
 if __name__ == '__main__':
