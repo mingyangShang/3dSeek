08d9747854ba74d133eb2d7451863d46a3b98ea4
mingyangShang <shang_ming_yang@163.com>
2017-08-13 14:06:46 +0800

    add modelnet10 feature

--- static/js/experiment.js
+++ static/js/experiment.js
@@ -10,7 +10,26 @@ window.chartColors = {
 };
 $(document).ready(function () {
 	var prCtx = document.getElementById("prChart").getContext('2d');
-	var config = {
+	var config40 = getPRConfig([1.0, 0.8987, 0.8734, 0.8458, 0.8082, 0.7830, 0.7593, 0.7347, 0.6956, 0.4399, 0.0348],
+                                [1.0, 0.9212, 0.916, 0.894, 0.8559, 0.8343, 0.8157, 0.8016, 0.7883, 0.7302, 0.0376],
+                                [1.0, 0.9294, 0.9202, 0.9021, 0.8656, 0.8456, 0.8282, 0.8126, 0.7977, 0.7655, 0.0371],
+                                'PR-Curve-ModelNet40');
+    window.prChart = new Chart(prCtx, config40);
+
+    var prCtx10 =  document.getElementById("prCanvas10").getContext('2d');
+    var config10 = getPRConfig([1.0, 0.9210, 0.9100, 0.9010, 0.8943, 0.8851, 0.8690, 0.8529, 0.8356, 0.7396, 0.1031],
+                                [1.0, 0.9222, 0.9122, 0.9070, 0.9028, 0.8988, 0.8789, 0.8663, 0.8516, 0.7951, 0.1093],
+                                [1.0, 0.9222, 0.9122, 0.9070, 0.9028, 0.8988, 0.8789, 0.8663, 0.8516, 0.7951, 0.1093],
+                                'PR-Curve-ModelNet10');
+    window.prChart10 = new Chart(prCtx10, config10);
+
+    $("#fancy-checkbox-primary").change(function() {
+        togglePrChartFill(this.checked);
+    });
+});
+
+function getPRConfig(P1, P2, P3, title){
+    var config = {
             type: 'line',
             data: {
                 labels: ["0", "0.1", "0.2", "0.3", "0.4", "0.5", "0.6", "0.7", "0.8", "0.9", "1.0"],
@@ -18,26 +37,20 @@ $(document).ready(function () {
                     label: "王曦阳",
                     backgroundColor: window.chartColors.red,
                     borderColor: window.chartColors.red,
-                    data: [
-                        1.0, 0.8987, 0.8734, 0.8458, 0.8082, 0.7830, 0.7593, 0.7347, 0.6956, 0.4399, 0.0348
-                    ],
+                    data: P1,
                     fill: true,
                 }, {
                     label: "商明阳",
                     fill: true,
                     backgroundColor: window.chartColors.blue,
                     borderColor: window.chartColors.blue,
-                    data: [
-                        1.0, 0.9212, 0.916, 0.894, 0.8559, 0.8343, 0.8157, 0.8016, 0.7883, 0.7302, 0.0376
-                    ],
+                    data: P2,
                 }, {
                 	label: "陆宏磊",
                     fill: true,
                     backgroundColor: window.chartColors.green,
                     borderColor: window.chartColors.green,
-                    data: [
-                        1.0, 0.9294, 0.9202, 0.9021, 0.8656, 0.8456, 0.8282, 0.8126, 0.7977, 0.7655, 0.0371
-                    ],
+                    data: P3,
                 }]
             },
             options: {
@@ -45,7 +58,7 @@ $(document).ready(function () {
                 maintainAspectRatio: true,
                 title:{
                     display:true,
-                    text:'PR-curve'
+                    text:title,
                 },
                 tooltips: {
                     mode: 'index',
@@ -73,17 +86,7 @@ $(document).ready(function () {
                 }
             }
         };
-        window.prChart = new Chart(prCtx, config);
-
-        $("#fancy-checkbox-primary").change(function() {
-        	togglePrChartFill(this.checked);
-		});
-});
-
-
-
-function randomScalingFactor(){
-	return (Math.random() > 0.5 ? 1.0 : -1.0) * Math.round(Math.random() * 100);
+    return config;
 }
 
 function togglePrChartFill(isFill){
@@ -91,4 +94,8 @@ function togglePrChartFill(isFill){
 		dataset.fill = isFill;
 	});
 	window.prChart.update();
+	window.prChart10.data.datasets.forEach((dataset) => {
+		dataset.fill = isFill;
+	});
+	window.prChart10.update();
 }
